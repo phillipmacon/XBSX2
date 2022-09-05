19613e8dd70a3db2a40b45b8a5cd27b94558b937
TheRhysWyrill <rhyswyrill@gmail.com>
2022-09-05 20:00:58 +0100

    Update Config.h
    Enable Full Texture Preloading by default.

--- pcsx2/Config.h
+++ pcsx2/Config.h
@@ -537,7 +537,7 @@ struct Pcsx2Config
 		AccBlendLevel AccurateBlendingUnit{AccBlendLevel::Basic};
 		CRCHackLevel CRCHack{CRCHackLevel::Automatic};
 		BiFiltering TextureFiltering{BiFiltering::PS2};
-		TexturePreloadingLevel TexturePreloading{TexturePreloadingLevel::Off};
+		TexturePreloadingLevel TexturePreloading{TexturePreloadingLevel::Full};
 		GSDumpCompressionMethod GSDumpCompression{GSDumpCompressionMethod::LZMA};
 		GSHardwareDownloadMode HWDownloadMode{GSHardwareDownloadMode::Enabled};
 		int Dithering{2};
